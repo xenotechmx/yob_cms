6159856124aacdb87d5bafb4e4c02d6d9e0fa96e
Zahit <zahit@xenotech.mx>
2020-04-21 15:45:14 -0500

    Firebase key without env

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace MetodikaTI\Http\Controllers;
 
 use Auth;
 use Carbon\Carbon;
-use GuzzleHttp\Psr7\Request;
+use GuzzleHttp;
 use Illuminate\Support\Facades\Mail;
 use LaravelQRCode\Facades\QRCode;
 use MetodikaTI\AppUser;
@@ -22,13 +22,13 @@ class TestController extends Controller
 {
     public function test(){
 
+        $API_ACCESS_KEY = 'AAAAEe2eHOE:APA91bF-_KM1CNWYNdTXTvdPkNcwFY_WmZICpwfsWunfHga1RoGzBto9znVEN_Q1KfBm7Cf8UKvzUJjOYeazkxUuZnkMbcK3v7a51pthHxTHgvjXluASOJ90nJJTzCr4ZZx7PGTr8al3';
+
         $notification = array();
         $notification["UUID"] = "diQnn-pXn40:APA91bFzTAE7LA-zNoEeWhLVOXgH0rzhVjE7aIhJMuxKMR6GPQ_ePEOFgjWpcTjP-Wgnm2AvFYpFl2IHM-rkIfEr2fKmsKqE7AoZtK6P7bSpMYbunnZvL6xuoh_KOjVxF_uRtKgTFAMF";
         $notification["TEXTO_NOTIFICACION"] = "Tienes un nuevo candidato en tu vacante Ingresa a la aplicación para más información.";
         $notification["TITLE"] = "Tienes un candidato postulado a tu vacante";
         
-        $API_ACCESS_KEY = env('SERVER_KEY_FIREBASE');
-
         $instructions = array();
         $instructions["action"] = (isset($notification["ACTION"]) ? $notification["ACTION"] : "");
 
@@ -66,5 +66,33 @@ class TestController extends Controller
         $result = json_decode($result);
 
         dump('resultado2: ',$result);
+
+
+        // $access_token = 'AAAAEe2eHOE:APA91bF-_KM1CNWYNdTXTvdPkNcwFY_WmZICpwfsWunfHga1RoGzBto9znVEN_Q1KfBm7Cf8UKvzUJjOYeazkxUuZnkMbcK3v7a51pthHxTHgvjXluASOJ90nJJTzCr4ZZx7PGTr8al3';
+
+        // $reg_id = 'diQnn-pXn40:APA91bFzTAE7LA-zNoEeWhLVOXgH0rzhVjE7aIhJMuxKMR6GPQ_ePEOFgjWpcTjP-Wgnm2AvFYpFl2IHM-rkIfEr2fKmsKqE7AoZtK6P7bSpMYbunnZvL6xuoh_KOjVxF_uRtKgTFAMF';
+
+        // $message = [
+        //     'notification' => [
+        //         'title' => 'Test Message',
+        //         'body' => "This is a test!",
+        //         "text" => "Text"
+        //     ],
+        //     'to' => $reg_id,
+        //     "priority"=> "high",
+        // ];
+
+        // $client = new GuzzleHttp\Client([
+        //     'headers' => [
+        //         'Content-Type' => 'application/json',
+        //         'Authorization' => 'key='.$access_token,
+        //     ]
+        // ]);
+
+        // $response = $client->post('https://fcm.googleapis.com/fcm/send',
+        //     ['body' => json_encode($message)]
+        // );
+
+        // echo $response->getBody();
     }
 }
